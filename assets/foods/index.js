81b44b44bcf88c456b6e2815a5d6e8387f98dd9e
Jpark <web.jpark@gmail.com>
2024-02-08 13:26:00 -0700

    organize the images

---
+++ assets/foods/index.js
@@ -0,0 +1,9 @@
+import burger from './burger.jpg';
+import curry from './curry.jpg';
+import dumplings from './dumplings.jpg';
+import macncheese from './macncheese.jpg';
+import pizza from './pizza.jpg';
+import schnitzel from './schnitzel.jpg';
+import tomatoSalad from './tomatoSalad.jpg';
+
+export { burger, curry, dumplings, macncheese, pizza, schnitzel, tomatoSalad };
