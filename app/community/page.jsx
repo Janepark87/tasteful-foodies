338f951756b946987cede775d3368636921b0621
Jpark <web.jpark@gmail.com>
2024-02-08 14:04:30 -0700

    implemented community page

--- app/community/page.jsx
+++ app/community/page.jsx
@@ -1,7 +1,35 @@
+import Image from 'next/image';
+import { communityIcon, eventsIcon, mealIcon } from '@/assets/icons/index';
+import styles from './styles/page.module.css';
+
 export default function CommunityPage() {
 	return (
 		<>
-			<h1>Community page</h1>
+			<section className={styles['hero-community']}>
+				<h1>
+					One shared passion: <span className={styles.highlight}>Food</span>
+				</h1>
+				<p>Join our community and share your favorite recipes!</p>
+			</section>
+
+			<section className={styles.section}>
+				<h2>Community Perks</h2>
+
+				<ul className={styles.perks}>
+					<li>
+						<Image src={mealIcon} alt="A delicious meal" priority />
+						<p>Share & discover recipes</p>
+					</li>
+					<li>
+						<Image src={communityIcon} alt="A crowd of people, cooking" priority />
+						<p>Find new friends & like-minded people</p>
+					</li>
+					<li>
+						<Image src={eventsIcon} alt="A crowd of people at a cooking event" priority />
+						<p>Participate in exclusive events</p>
+					</li>
+				</ul>
+			</section>
 		</>
 	);
 }
---
+++ assets/icons/index.js
@@ -0,0 +1,6 @@
+import communityIcon from './community.png';
+import eventsIcon from './events.png';
+import mealIcon from './meal.png';
+import logo from './logo.png';
+
+export { communityIcon, eventsIcon, mealIcon, logo };
