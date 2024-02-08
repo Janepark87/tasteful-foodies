1bd38dc3de37954377c1e4eb45de57e850bda5dd
Jpark <web.jpark@gmail.com>
2024-02-08 12:23:51 -0700

    implemented the main header and header bg

--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -1,14 +1,30 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import styles from './styles/MainHeader.module.css';
 import logo from '@/assets/logo.png';
+import MainHeaderBg from './MainHeaderBg';
 
 export default function MainHeader() {
 	return (
-		<header>
-			<Link href="/" title="A plate with food on it">
-				<img src={logo.src} alt="Logo image" />
-				<span>Tasteful Foodies</span>
-			</Link>
-			<nav></nav>
-		</header>
+		<>
+			<MainHeaderBg />
+			<header className={styles.header}>
+				<Link href="/" className={styles.logo} title="A plate with food on it">
+					<Image src={logo} alt="Logo image" priority />
+					<span>Tasteful Foodies</span>
+				</Link>
+
+				<nav className={styles.nav}>
+					<ul>
+						<li>
+							<Link href="/meals">Meals</Link>
+						</li>
+						<li>
+							<Link href="/community">Community</Link>
+						</li>
+					</ul>
+				</nav>
+			</header>
+		</>
 	);
 }
